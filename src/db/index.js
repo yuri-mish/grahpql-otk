9fb4ffdf5692ffff61118aede9a21adcead3260f
yuri-mish <admin@starsam.net>
2021-01-22 14:05:55 +0100

    aaa

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,11 @@
 const { Pool } = require('pg')
 var nano = require('nano')
 
-const pool = new Pool({
-  host:"starsam.net",
-    database:"test_db",
-    user:"postgres",
-    password:"qq031018"
-})
+const config = require('./config');
 
-const poolSync = new Pool({
-  host:"starsam.net",
-    database:"test_db",
-    user:"postgres",
-    password:"qq031018"
-})
+const pool = new Pool(config.db)
+const poolSync = new Pool(config.db)
+
 module.exports = { 
   query: (text, params, callback) => {
     return pool.query(text, params, callback)
@@ -21,6 +13,5 @@ module.exports = {
   querySync: (text, params, callback) => {
     return poolSync.query(text, params, callback)
   },
-  couch: nano('https://admin:NodeArt9@couch.vioo.com.ua')
-  
-}
+  couch: nano( `https://${config.couch.user}:${config.couch.password}@${config.couch.host}`),
+};
